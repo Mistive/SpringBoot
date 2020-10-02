dc245f1998d2bc280c1dc1200b0d87d7e154bcbc
pms940725 <pms940725@naver.com>
2020-10-02 23:54:59 +0900

    Spring Boot jsp 적용하기_2  * taglib 사용해서 적용하기  * ModelAndView 객체 적용하기

--- src/main/java/com/example/demo2/test/controller/HelloController.java
+++ src/main/java/com/example/demo2/test/controller/HelloController.java
@@ -1,16 +1,26 @@
 package com.example.demo2.test.controller;
 
-        import org.springframework.stereotype.Controller;
-        import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-public class HelloController {
+import java.util.ArrayList;
+import java.util.List;
 
-    @RequestMapping(value="/halo")
-    public String testapp() {
+        @Controller
+        public class HelloController {
 
-        return "test";
-
+            @RequestMapping("/ttt")
+            public ModelAndView ttt() throws Exception {
+                ModelAndView mav = new ModelAndView("test");
+                mav.addObject("name", "Mistive");
+                List<String> testList = new ArrayList<String>();
+                testList.add("a");
+                testList.add("b");
+                testList.add("c");
+                mav.addObject("list", testList);
+        return mav;
     }
+
 
 }
